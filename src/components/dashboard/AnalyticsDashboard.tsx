a9394e666df61917e9bf238921aeb57c1c297b4d
Jesus Vazquez <tonio4340406@gmail.com>
2026-03-05 15:10:35 -0800

    Messages Card Structure

--- src/components/dashboard/AnalyticsDashboard.tsx
+++ src/components/dashboard/AnalyticsDashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ChatDuration from "./ChatDuration";
+import UniqueUsersCard from "./UniqueUsersCard";
 
 const AnalyticsDashboard = () => {
   return (
     <div className="w-85">
       <ChatDuration />
+      <UniqueUsersCard />
     </div>
   );
 };
