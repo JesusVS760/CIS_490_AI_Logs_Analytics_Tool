6a27627a6543fa27c5ec32ff0b201b201e73275f
Jesus Vazquez <tonio4340406@gmail.com>
2026-03-12 11:25:51 -0700

    WordCloud UI change & Random util func update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const randomColor = () =>
-  "#" + Math.floor(Math.random() * 16777215).toString(16);
+export const randomColor = () => {
+  const r = Math.floor(Math.random() * 201);
+  const g = Math.floor(Math.random() * 201);
+  const b = Math.floor(Math.random() * 201);
+
+  const toHex = (x: number) => x.toString(16).padStart(2, "0");
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+};
