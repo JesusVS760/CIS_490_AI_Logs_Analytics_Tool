71802898ff7aaf36371f689a061e10ddd49b0c79
Zyrus Tyson <zy@Zyruss-MacBook-Air.local>
2026-03-25 20:19:32 -0700

    Created the componenets folder and created the total number of users per homework under AssignmentsUsersChart.tsx

--- src/app/assignments/page.tsx
+++ src/app/assignments/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AssignmentsUsersChart from "../../components/assignments/AssignmentsUsersChart";
+
+console.log("AssignmentsUsersGraph:", AssignmentsUsersChart);
 
 const AssignmentsPage = () => {
-  return <div>AssignmentsPage</div>;
+  return (
+    <div>
+      <h1>Assignments</h1>
+      <AssignmentsUsersChart />
+    </div>
+  );
 };
 
 export default AssignmentsPage;
---
+++ src/components/assignments/AssignmentsUsersChart.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const AssignmentsUsersChart = () => {
+  return <div>Graph here will be implemented. This is a test to ensure the functionality of this file.</div>;
+};
+
+export default AssignmentsUsersChart;
