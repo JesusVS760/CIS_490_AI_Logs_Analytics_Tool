8339ca0b248cef2706c63dae1771368d243fe45b
JesusVA760 <tonio4340406@gmail.com>
2026-04-22 23:41:39 -0700

    remove middlewarePrefetch experiment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@libsql/client", "better-sqlite3"],
-  experimental: {
-    middlewarePrefetch: "strict",
-  },
 };
 
 export default nextConfig;
